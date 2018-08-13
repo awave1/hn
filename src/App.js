ab626a91027b07bdd024a1de54e6244ab1f100b3
awave1 <artgolovin96@gmail.com>
2018-08-12 23:43:50 -0600

    adds simple hacker news api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topStories: [],
+    };
+  }
+
+  async componentWillMount() {
+    const { data: topStories } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    this.setState({ topStories });
+  }
+
   render() {
     return (
       <div className="App">
+        <ul>
+          {
+            this.state.topStories.map(s => <li>{s}</li>)
+          }
+        </ul>
       </div>
     );
   }
