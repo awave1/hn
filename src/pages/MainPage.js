e9c25eaf8152ba049042184773bcc278d730e824
awave1 <artgolovin96@gmail.com>
2018-08-18 12:49:36 -0600

    changed component file structure

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { getTopStories } from '../api/stories';
 
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const topStories = await getTopStories();
+    this.setState({
+      stories: topStories
+    });
+  }
+
   render() {
     return(
-      <h1>MainPage</h1>
+      <ul>
+        {
+          this.state.stories.map(story => <li key={story.id}>{story.title}</li>)
+        }
+      </ul>
     )
   }
 }
